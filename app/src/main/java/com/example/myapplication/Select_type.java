ced581a840181dd257c48586761f1bf180d2de5e
god-of-jesters <ryzovdima@gmail.com>
2023-05-01 23:43:45 +0300

    First

---
+++ app/src/main/java/com/example/myapplication/Select_type.java
@@ -0,0 +1,52 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Select_type extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_type);
+        Button ac = findViewById(R.id.button2);
+        Button speed = findViewById(R.id.button3);
+        Button count = findViewById(R.id.button4);
+
+        ac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Select_type.this, Accuracy.class);
+                startActivity(intent);
+            }
+        });
+
+        speed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Select_type.this, Speed.class);
+                startActivity(intent);
+            }
+        });
+
+        count.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Select_type.this, Accuracy.class);
+                startActivity(intent);
+            }
+        });
+
+        ac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Select_type.this, Accuracy.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
